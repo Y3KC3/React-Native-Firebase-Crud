2e77305579223c3b793f5154dce63134d5117feb
Melvin Colmenares <76851489+Y3KC3@users.noreply.github.com>
2023-01-29 16:49:37 -0400

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABaPulQ3dnIGgYcIHjT9R4fU_I_bzYACk",
-  authDomain: "react-native-firebase-cr-64382.firebaseapp.com",
-  projectId: "react-native-firebase-cr-64382",
-  storageBucket: "react-native-firebase-cr-64382.appspot.com",
-  messagingSenderId: "758165404428",
-  appId: "1:758165404428:web:fa843c4d4c681e5e91a729"
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTH-DOMAIN.firebaseapp.com",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE-BUCKET.appspot.com",
+  messagingSenderId: "MESSAGIN-SENDER-ID",
+  appId: "YOUR-APP-ID"
 };
 
 initializeApp(firebaseConfig);
